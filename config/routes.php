75cd0a010decc840fa349887c2b16c17d4b1b722
Ihor110986 <ihor110986@gmail.com>
2022-04-09 19:58:02 +0300

    Update bootstrap.php and docker.compose.yml

--- config/routes.php
+++ config/routes.php
@@ -4,4 +4,6 @@ return [
     '/' => \App\MyControllers\MainController::class,
     '/todo' => \App\MyControllers\TodoController::class,
     '/not-found' =>  \App\MyControllers\NotFoundController::class,
+    '/authorization' => \App\MyControllers\AuthorizationController::class,
+    '/registeration' => \App\MyControllers\RegistrationController::class,
 ];
